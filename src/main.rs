038044aebca4a9682f04473825549a4104d631b7
h4ck4l1 <sohailmd123@gmail.com>
2024-08-22 01:47:12 +0530

    add

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,4 @@
 #![allow(unused_imports)]
-mod roman_to_integer;
-use roman_to_integer::roman_to_int;
 
-fn main() {
-    println!("{}",roman_to_int("MCMXCIV".to_string()));
-}
+fn main() {}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn first_test() {
-        assert_eq!(3,roman_to_int(String::from("III")));
-    }
-
-    #[test]
-    fn second_test() {
-        assert_eq!(58,roman_to_int(String::from("LVIII")));
-    }
-
-    #[test]
-    fn third_test() {
-        assert_eq!(1994,roman_to_int(String::from("MCMXCIV")));
-    }
-}
