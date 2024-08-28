2a1643ac009d75aaee109c674523d817d67dd3ec
h4ck4l1 <sohailmd123@gmail.com>
2024-08-28 06:25:55 +0530

    aadd

--- src/merge_two_sorted_lists.rs
+++ src/merge_two_sorted_lists.rs
@@ -30,5 +30,17 @@ impl ListNode {
 }
 
 pub fn make_list(list: Vec<i32>) -> Option<Box<ListNode>> {
-    
+    if list.len() == 0 {
+        return None;
+    }
+    else if list.len() == 1 {
+        return Some(Box::new(ListNode::new_from_list(&list)));
+    }
+    let mut result_node: ListNode = ListNode::new_from_list(&list);
+    let mut itr: &mut ListNode = &mut result_node;
+    for &i in &list[1..] {
+        itr.append_to_list(&list,i as usize);
+        itr = itr.next.as_mut().unwrap();
+    }
+    Some(Box::from(result_node))
 }
