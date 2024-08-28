1a436c3dd86edf2669f7cc9ca23b6e141a066ccf
h4ck4l1 <sohailmd123@gmail.com>
2024-08-28 06:31:04 +0530

    add

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 #![allow(unused_imports)]
 
 mod merge_two_sorted_lists;
-use merge_two_sorted_lists::make_list;
+use merge_two_sorted_lists::{ListNode,make_list,merge_two_lists};
 
 fn main() {
-    let some_node = make_list(vec![1,2,4]);
-    println!("{:?}",some_node.as_ref().unwrap().val);
-    println!("{:?}",some_node.as_ref().unwrap().next.as_ref().unwrap().val);
-    println!("{:?}",some_node.unwrap().next.unwrap().next.unwrap().val);
+    
 }
 
 #[cfg(test)]
 mod tests {
-    use merge_two_sorted_lists::{make_list, merge_two_sorted_lists, ListNode};
 
     use super::*;
 
@@ -20,7 +16,7 @@ mod tests {
     fn first_test() {
         assert_eq!(
             make_list(vec![1,1,2,3,4,4]),
-            merge_two_sorted_lists(make_list(vec![1,2,4]),make_list(vec![1,3,4]))
+            merge_two_lists(make_list(vec![1,2,4]),make_list(vec![1,3,4]))
         );
     }
 
@@ -28,7 +24,7 @@ mod tests {
     fn second_test() {
         assert_eq!(
             make_list(vec![]),
-            merge_two_sorted_lists(make_list(vec![]),make_list(vec![]))
+            merge_two_lists(make_list(vec![]),make_list(vec![]))
         );
     }
 
@@ -36,7 +32,7 @@ mod tests {
     fn third_test() {
         assert_eq!(
             make_list(vec![0]),
-            merge_two_sorted_lists(make_list(vec![]),make_list(vec![0]))
+            merge_two_lists(make_list(vec![]),make_list(vec![0]))
         );
     }
 
@@ -44,7 +40,7 @@ mod tests {
     fn fourth_test() {
         assert_eq!(
             make_list(vec![1,2]),
-            merge_two_sorted_lists(make_list(vec![2]),make_list(vec![1]))
+            merge_two_lists(make_list(vec![2]),make_list(vec![1]))
         );
     }
 }
--- src/merge_two_sorted_lists.rs
+++ src/merge_two_sorted_lists.rs
@@ -3,13 +3,15 @@
 #![allow(unused_imports)]
 
 
-
+#[derive(Debug,Clone,PartialEq,Eq)]
 pub struct ListNode {
     val: i32,
     next: Option<Box<ListNode>>
 }
 
 impl ListNode {
+
+    #[inline]
     fn new(val: i32) -> Self {
         ListNode {
             val,
@@ -20,10 +22,13 @@ impl ListNode {
 
 
 impl ListNode {
+
+
     fn new_from_list(list: &Vec<i32>) -> Self {
         ListNode::new(list.get(0).unwrap().to_owned())
     }
 
+    #[inline]
     fn append_to_list(&mut self, list: &Vec<i32>, index: usize) {
         self.next = Some(Box::new(ListNode::new(list.get(index).unwrap().to_owned())))
     }
@@ -43,4 +48,9 @@ pub fn make_list(list: Vec<i32>) -> Option<Box<ListNode>> {
         itr = itr.next.as_mut().unwrap();
     }
     Some(Box::from(result_node))
+}
+
+
+pub fn merge_two_lists(list1: Option<Box<ListNode>>,list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    Some(Box::new(ListNode::new(0)))
 }
